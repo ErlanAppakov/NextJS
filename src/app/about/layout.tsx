562d9ad7aa23ab5849b4aebbe340d71f4ca14134
Erlan <eraappakov16@gmail.com>
2026-03-17 06:04:12 +0300

    Clean up project for next-course

--- src/app/about/layout.tsx
+++
@@ -1,1 +0,0 @@
-export default function AboutLayout({ children }: { children: React.ReactNode }) { return (<div className="container mx-auto p-4"> {children} </div>); }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,10 @@
 
 
 
-export default async function Home() {
-  let response=await fetch('https://jsonplaceholder.typicode.com/users')
-  .then(res => res.json())
-  console.log(response);
-  
-  return(
-    response.map((user:any)=>{
-      return(
-        <div key={user.id}>{user.username}</div>
-      )
-    })
-  )
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Next.js</h1>
+    </div>
+  );
 }
